109dea2ec1b0acc26936f7ae3e5e1228a0522b3f
IlyaRemorsA <blackbaggetjack@gmail.com>
2022-08-19 14:16:12 +0300

    main

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    F29C-1668
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      aa: 'dwadawd',
+      ww:   "dwawww",
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+ .aa{
+
+ }
+</style>
